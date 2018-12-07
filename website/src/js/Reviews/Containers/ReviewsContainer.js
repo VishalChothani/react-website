8d4ebe114fa3de72d16f5febe7d2368db026720d
Neha Chothani <nehachothani@Nehas-MacBook-Air.local>
2018-12-07 17:49:32 +0530

    Fix mobile version of reviews

--- website/src/js/Reviews/Containers/ReviewsContainer.js
+++ website/src/js/Reviews/Containers/ReviewsContainer.js
@@ -1,24 +1,29 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import ReviewsComponent from '../Component/ReviewsComponent';
+import configurationService from '../../Common/ConfigurationService';
 
 class ReviewsContainer extends Component{
   render(){
     const {
       reviewsData,
     } = this.props;
+    const isInMobile = configurationService.isInMobile();
     return(
       <section className="reviews align-center padding-v bootstrap-grid">
         <div className="h2 margin-bot">Reviews</div>
-        <div className="reviews-section card-container margin-bot">
-          <div className="container">
-            <div className="row">
-              <div className="col-xs-12 col-sm-offset-2 col-sm-8">
-                <ReviewsComponent reviewsData={reviewsData} />
+        { isInMobile && <ReviewsComponent reviewsData={reviewsData} /> }
+        { !isInMobile && 
+          <div className="reviews-section card-container margin-bot">
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-offset-2 col-sm-8">
+                  <ReviewsComponent reviewsData={reviewsData} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        }
       </section>
     )
   }
