27bd7d2b1c544948e6f0c743e6280a5e26c3e5c4
Vishal Chothani <vishal.k.chothani@gmail.com>
2019-08-08 20:12:32 -0700

    Added coming soon

--- website/src/js/KidsCollection/Container/KidsCollectionContainer.js
+++ website/src/js/KidsCollection/Container/KidsCollectionContainer.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import CollectionContainer from '../../Collection/Container/CollectionContainer';
-import {CONSTANTS} from '../../Common/Constants';
+// import CollectionContainer from '../../Collection/Container/CollectionContainer';
+// import {CONSTANTS} from '../../Common/Constants';
+import comingSoon from '../../../img/comingSoon.png';
 
 class KidsCollectionContainer extends Component{
   render(){
-    const {
-      kidsData,
-    } = this.props;
+    // const {
+    //   kidsData,
+    // } = this.props;
     return(
       <section className="cateogry-collection">
         <div className="h2 margin-v align-center capitalize">Kids</div>
-        <CollectionContainer collectionData={kidsData} category={CONSTANTS.pages.kids} />
+        {/* <CollectionContainer collectionData={kidsData} category={CONSTANTS.pages.kids} /> */}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-offset-2 col-md-8">
+              <img className="width-full" src={comingSoon} />
+            </div>
+          </div>
+        </div>
       </section>
     )
   }
--- website/src/js/MensCollection/Container/MensCollectionContainer.js
+++ website/src/js/MensCollection/Container/MensCollectionContainer.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import CollectionContainer from '../../Collection/Container/CollectionContainer';
-import {CONSTANTS} from '../../Common/Constants';
+// import CollectionContainer from '../../Collection/Container/CollectionContainer';
+// import {CONSTANTS} from '../../Common/Constants';
+import comingSoon from '../../../img/comingSoon.png';
 
 class MensCollectionContainer extends Component{
   render(){
-    const {
-      menData,
-    } = this.props;
+    // const {
+    //   menData,
+    // } = this.props;
     return(
-      <section className="cateogry-collection">
+      <section className="cateogry-collection bootstrap-grid">
         <div className="h2 margin-v align-center capitalize">Mens</div>
-        <CollectionContainer collectionData={menData} category={CONSTANTS.pages.men} />
+        {/* <CollectionContainer collectionData={menData} category={CONSTANTS.pages.men} /> */}
+        <div className="container">
+          <div className="row">
+            <div className="col-md-offset-2 col-md-8">
+              <img className="width-full" src={comingSoon} />
+            </div>
+          </div>
+        </div>
       </section>
     )
   }
