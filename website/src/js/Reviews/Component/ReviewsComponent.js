e5c5dc5f7df6c4f2e97959ef622b455df8b5db31
Vishal Chothani <vishal.k.chothani@gmail.com>
2019-08-11 20:44:36 -0700

    Adding reviews

--- website/src/js/Reviews/Component/ReviewsComponent.js
+++ website/src/js/Reviews/Component/ReviewsComponent.js
@@ -13,6 +13,7 @@ export default class ReviewsComponent extends Component{
           reviewsData.map((reviewsData) => (
             <div className="align-center" key={reviewsData.comment}>
               <img className="review-img responsive-img margin-bot-sm align-center" src={reviewsData.userImg} alt={reviewsData.alt} />
+              <div className="h4 margin-bot">{reviewsData.name}</div>
               <div className="review-comment margin-h">{reviewsData.comment}</div>
             </div>
           )) 
--- website/src/js/Reviews/Reducers/ReviewsReducer.js
+++ website/src/js/Reviews/Reducers/ReviewsReducer.js
@@ -1,24 +1,30 @@
 import nehaPhoto from '../../../img/neha.jpeg';
+import sanskrutiPhoto from '../../../img/sanskruti.jpg';
+import nehaRuggerPhoto from '../../../img/neha_rugger.jpg';
+import pratikPhoto from '../../../img/pratik.jpg';
 
 export default () => {
   return [
     {
-
-      userImg: nehaPhoto,
+      userImg: sanskrutiPhoto,
+      name: "Sanskruti Balgude - Marathi actor",
       alt: "Neha's photo",
-      comment: "1Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say",
+      comment: "I have been wearing Neha Chothani's label. I have been wearing her outfit for a long time now. I really think they are pretty and elegant. I'll be wearing more of them soon",
+    },
+    
+    {
+
+      userImg: nehaRuggerPhoto,
+      name: "Neha Rugger - Rugby player",
+      alt: "Neha's photo",
+      comment: "Highly recommend this cute little dress shop! I usually dislike shopping for dresses due to my curvy figure that makes dress fittings a nightmare, but Neha made this such a fun and easy experience!! She helped me to get in/out of the dresses and give her honest opinion about it.I told Neha roughly what Im looking for, and she immediately came up with few great options. I tried one on and LOVED it. It was flattering and looked beautiful from every angle. The halter top was a bit too loose, so she tailored it in 24 hours for a very reasonable price! I was so impressed. I had prepared myself for a frustrating few hours of trying on dresses that don't fit, but instead I finished my shopping in less than an hour, and walked out feeling happy and relieved. Look no further if you need beautiful dresses at a reasonable price, with unbeatable friendly and knowledgeable customer service. Thank you, Neha!",
     },
 
     {
-      userImg: nehaPhoto,
+      userImg: pratikPhoto,
+      name: "Pratik Jain",
       alt: "Neha's photo",
-      "comment": "2Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say",
-    },
-
-    {
-      userImg: nehaPhoto,
-      alt: "Neha's photo",
-      comment: "3Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say Very nice things to say",
+      "comment": "Its always a little uncomfortable when you are a guy buying gifts in a store intented for women. Neha made an extra effort to make sure I was comfortable, informed and taught me about different types of fabrics, texture etc. Thanks, my wife is going to love her new dress!",
     },
   ]
 }
