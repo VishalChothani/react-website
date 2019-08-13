371ca0e4e1df14528b1a1cb3c182da9023699d55
Vishal Chothani <vishal.k.chothani@gmail.com>
2019-08-12 22:08:51 -0700

    Updates

---
+++ precache-manifest.e8cbe2b76fd555e97e27e8f279dc58cf.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "34c27f95e4d24eb76314e5c8d5f4c1db",
+    "url": "/static/media/pinkCapeSleevesGown.34c27f95.jpg"
+  },
+  {
+    "revision": "c0a14aa9595bbd5459f1",
+    "url": "/static/css/main.9a42efda.chunk.css"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "5c72e9d1846706cf2ef5",
+    "url": "/static/js/1.5c72e9d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5cd4192f3b0ddb1296ec7d274120dca2",
+    "url": "/static/media/card.5cd4192f.jpg"
+  },
+  {
+    "revision": "5a4e95375a24ab605b11a908802b4396",
+    "url": "/static/media/comingSoon.5a4e9537.png"
+  },
+  {
+    "revision": "71362201d3e34b58bf2f23509d5e04cf",
+    "url": "/static/media/greyJacket.71362201.jpg"
+  },
+  {
+    "revision": "a9e311a2c69a3b71c225b4a5ed7699dd",
+    "url": "/static/media/oneShoulderDress.a9e311a2.jpg"
+  },
+  {
+    "revision": "c8bf58d1674ea6a9d27cdcbb8611a996",
+    "url": "/static/media/sanskruti.c8bf58d1.jpg"
+  },
+  {
+    "revision": "e80b11f499a1ef5c79472b0574d5f3d3",
+    "url": "/static/media/neha_rugger.e80b11f4.jpg"
+  },
+  {
+    "revision": "bb89038b5199156b9e95c97ca5ea310c",
+    "url": "/static/media/pratik.bb89038b.jpg"
+  },
+  {
+    "revision": "b8c9c02062950a1a78401851d9d1fc98",
+    "url": "/static/media/whiteRed.b8c9c020.jpg"
+  },
+  {
+    "revision": "47370a6352d5e4e1ba672ceaf9885177",
+    "url": "/static/media/whiteRedLarge.47370a63.jpg"
+  },
+  {
+    "revision": "a2b5a96703fccf1e56e2ae9b9d0ad65e",
+    "url": "/static/media/brocadeDress.a2b5a967.jpg"
+  },
+  {
+    "revision": "b6138d0b82bb858ee9f3558fae88016d",
+    "url": "/static/media/crossOfStyle.b6138d0b.jpg"
+  },
+  {
+    "revision": "05fa7de89421adc88e8a7ffd4c6b70b1",
+    "url": "/static/media/goldBlack.05fa7de8.jpg"
+  },
+  {
+    "revision": "1d1e9f9ac7263ca99b26fa0dfd9aeda2",
+    "url": "/static/media/goldMirrorWorkLehenga.1d1e9f9a.jpg"
+  },
+  {
+    "revision": "23f136c7f56b861cba34bba108629211",
+    "url": "/static/media/mustardLehengaTop.23f136c7.jpg"
+  },
+  {
+    "revision": "2972d243e6ef3dd04933b7456bca199c",
+    "url": "/static/media/orangeFlowerPrint.2972d243.jpg"
+  },
+  {
+    "revision": "c0a14aa9595bbd5459f1",
+    "url": "/static/js/main.c0a14aa9.chunk.js"
+  },
+  {
+    "revision": "70b2781e1ef12b0e66ca4325bba1a30f",
+    "url": "/static/media/pinkJacket.70b2781e.jpg"
+  },
+  {
+    "revision": "165aca6f0a7ec2fb5d05e13cd9ff2dae",
+    "url": "/static/media/brocadeDressLarge.165aca6f.jpg"
+  },
+  {
+    "revision": "eb8c19697ec8251a887a37d5c02f06d5",
+    "url": "/static/media/crossOfStyleLarge.eb8c1969.jpg"
+  },
+  {
+    "revision": "18ff6609b306d464a347272edbb42918",
+    "url": "/static/media/goldBlackLarge.18ff6609.jpg"
+  },
+  {
+    "revision": "87b2d46025ad8f5fbf39d0668e1f10d7",
+    "url": "/static/media/goldMirrorWorkLehengaLarge.87b2d460.jpg"
+  },
+  {
+    "revision": "b24a3046e7b2efb61646bb1f63bc3cdf",
+    "url": "/static/media/mustardLehengaTopLarge.b24a3046.jpg"
+  },
+  {
+    "revision": "23b4efda5419c90233fd7efadecde955",
+    "url": "/static/media/orangeFlowerPrintLarge.23b4efda.jpg"
+  },
+  {
+    "revision": "7163a522db8a76dcbc8ed4f0a1890f6e",
+    "url": "/static/media/pinkCapeSleevesGownLarge.7163a522.jpg"
+  },
+  {
+    "revision": "e91b82dde10868f4741a44dd2bffd4b2",
+    "url": "/static/media/pinkJacketLarge.e91b82dd.jpg"
+  },
+  {
+    "revision": "80711c7a20762409e98af7e19217199e",
+    "url": "/static/media/label.80711c7a.jpeg"
+  },
+  {
+    "revision": "8792b22e44eaa701da374a4686554483",
+    "url": "/static/media/cover.8792b22e.jpg"
+  },
+  {
+    "revision": "38d41988bbf09cee8a31252a9785105b",
+    "url": "/static/media/logo.38d41988.png"
+  },
+  {
+    "revision": "fcf2983466d5bdad9a11ac4a50263156",
+    "url": "/static/media/map.fcf29834.png"
+  },
+  {
+    "revision": "6ab4e42bcb70a71724468d6d96ee5cfc",
+    "url": "/static/media/pre-loader-background.6ab4e42b.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5c72e9d1846706cf2ef5",
+    "url": "/static/css/1.069ecbcb.chunk.css"
+  },
+  {
+    "revision": "72c9ae81f45a5ae7aadc83b672545bf2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-website/precache-manifest.a7e0537de99b3c6ab377bd43690b2d28.js"
+  "/precache-manifest.e8cbe2b76fd555e97e27e8f279dc58cf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
